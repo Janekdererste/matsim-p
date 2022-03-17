b8433bf5141864bad4420e5e8b311f1b216cd549
Janekdererste <janek.laudan@mailbox.org>
2022-03-17 15:49:40 +0100

    Create a Repository to play around with MPI and MATSim.

--- src/main/java/org/matsim/parallel/RunMinimalMPIExample.java
+++ src/main/java/org/matsim/parallel/RunMinimalMPIExample.java
@@ -2,14 +2,41 @@ package org.matsim.parallel;
 
 import mpi.MPI;
 import mpi.MPIException;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.controler.OutputDirectoryHierarchy;
+import org.matsim.core.controler.events.AfterMobsimEvent;
+import org.matsim.core.controler.listener.AfterMobsimListener;
+import org.matsim.core.controler.listener.ControlerListener;
+import org.matsim.core.scenario.ScenarioUtils;
+import org.matsim.core.utils.io.IOUtils;
+import org.matsim.examples.ExamplesUtils;
+
+
 
 public class RunMinimalMPIExample {
 
+
     public static void main(String[] args) throws MPIException {
 
+        var version = System.getProperty("java.version");
+        System.out.println("Java-Version: " + version);
+
         MPI.Init(args);
+        var rank = MPI.COMM_WORLD.getRank();
 
-
+        System.out.println("before getTestScenario");
+        var equil = ExamplesUtils.getTestScenarioURL("equil");
+        System.out.println("before load config");
+        var config = ConfigUtils.loadConfig(IOUtils.extendUrl(equil, "config.xml"));
+        config.qsim().setNumberOfThreads(1);
+        config.controler().setOverwriteFileSetting(OutputDirectoryHierarchy.OverwriteFileSetting.deleteDirectoryIfExists);
+        config.controler().setOutputDirectory("./output/equil-" + rank);
+        config.controler().setLastIteration(100);
+        var scenario = ScenarioUtils.loadScenario(config);
+        var controler = new Controler(scenario);
+        controler.run();
 
         MPI.Finalize();
     }
